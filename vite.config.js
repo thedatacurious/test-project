e97b178ee75d85ee1b2ffadb33e4dcc03d1acf63
Matt Osborn <mattjosborn@gmail.com>
2022-12-04 10:37:04 -0600

    fix build target for bigint support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+const ESBUILD_MODULES_TARGET = [
+  "es2020",
+  "edge88",
+  "firefox78",
+  "chrome87",
+  // vite default setting is to support safari13
+  // https://github.com/vitejs/vite/blob/134ce6817984bad0f5fb043481502531fee9b1db/packages/vite/src/node/constants.ts#L20-L26
+  // safari13 doesn't support bigint so we can up the version to 14.
+  // https://caniuse.com/bigint
+  "safari14",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: ESBUILD_MODULES_TARGET,
+    },
+  },
+  build: {
+    target: ESBUILD_MODULES_TARGET,
+  },
+});
