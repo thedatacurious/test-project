1dab82aac176f2d2265b1104f173191258c2bccf
Pere <peroc79@gmail.com>
2022-12-31 11:49:33 +0100

    CSV deps

--- src/Components/Counter.svelte
+++ src/Components/Counter.svelte
@@ -2,6 +2,8 @@
 <script>
 import {scaleLinear, scaleBand} from 'd3-scale';
 
+import energyDataCsv from './../data/energyData.csv';
+
 	import {max} from 'd3-array'
   let count = 0
   const increment = () => {
